16e457b2a5b3dda2ab48fc1bd51a31234dda74bf
flagersgit <flagersmail@gmail.com>
2023-06-07 11:54:12 -0400

    Added constants for macOS 14 support

--- KDKlessWorkaround/kern_start.cpp
+++ KDKlessWorkaround/kern_start.cpp
@@ -42,6 +42,6 @@ PluginConfiguration ADDPR(config) {
     bootargBeta,
     arrsize(bootargBeta),
     KernelVersion::BigSur,
-    KernelVersion::Ventura,
+    KernelVersion::Sonoma,
     pluginStart
 };
